b3031b18f8692880d3806cad4a37fff5804df3c8
Andrew Shen <shenandrew95@gmail.com>
2019-05-15 14:11:03 -0400

    additions of jsoup for scrapping stock data, possible replacement for weird alphavantage api which sometimes is buggy

--- src/main/java/StockBot/Stock/Stock.java
+++ src/main/java/StockBot/Stock/Stock.java
@@ -26,7 +26,7 @@ public class Stock {
   		this.stockAbv = stockAbv;
   	}
 
-	public void getData() {
+	public StockData getData() {
 		AlphaVantageConnector apiConnector = new AlphaVantageConnector(apiKey, timeout);
 		TimeSeries stockTimeSeries = new TimeSeries(apiConnector);
 
@@ -44,9 +44,12 @@ public class Stock {
 			System.out.println("high:   " + stock.getHigh());
 			System.out.println("low:    " + stock.getLow());
 			System.out.println("close:  " + stock.getClose());
-			System.out.println("volume: " + stock.getVolume());    
+			System.out.println("volume: " + stock.getVolume());
+
+			return stock;
 		} catch (AlphaVantageException e) {
 			System.out.println("something went wrong");
 		}
+		return null;
 	}
 }
